1dc42191fe7716cab19f6dc664e7fba7e6c5c5ce
Christentimmy <timmychris09@gmail.com>
2025-10-01 02:10:02 +0300

    index modified

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { setupSocket } from "./config/socket";
 import authRoutes from "./routes/auth_routes";
 import messageRoutes from "./routes/message_route";
 import supportTicketRoutes from "./routes/support_ticket_routes";
+import storyRoutes from "./routes/story_routes";
 import userRoutes from "./routes/user_routes";
 
 dotenv.config();
@@ -21,6 +22,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/message', messageRoutes);
+app.use('/api/story', storyRoutes);
+app.use('/api/support', supportTicketRoutes);
 
 connectToDatabase();
 
